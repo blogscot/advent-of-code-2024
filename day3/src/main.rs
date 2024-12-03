30dc2bf68dd21978e48f50883ab96c52d3bda8c8
blogscot <iain.diamond@gmail.com>
2024-12-03 11:54:51 +0000

    Refactor part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -14,23 +14,19 @@ fn solve_part1(memory: &str) -> u64 {
 fn solve_part2(memory: &str) -> u64 {
     let mut sum = 0;
     let mut state = State::On;
-
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)").unwrap();
-    let re2 = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let matches = re.captures_iter(memory).map(|m| m.get(0).unwrap().as_str());
-    for m in matches {
-        match m {
+    for (operation, first, second) in re
+        .captures_iter(memory)
+        .map(|m| (m.get(0).unwrap().as_str(), m.get(1), m.get(2)))
+    {
+        match operation {
             "do()" => state = State::On,
             "don't()" => state = State::Off,
             _ => {
                 if state == State::On {
-                    re2.captures(m).map(|c| c.extract()).into_iter().for_each(
-                        |(_, [first, second])| {
-                            let first = first.parse::<u64>().unwrap();
-                            let second = second.parse::<u64>().unwrap();
-                            sum += first * second;
-                        },
-                    )
+                    let first = first.unwrap().as_str().parse::<u64>().unwrap();
+                    let second = second.unwrap().as_str().parse::<u64>().unwrap();
+                    sum += first * second;
                 }
             }
         }
