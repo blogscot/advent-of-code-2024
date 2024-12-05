f0ec253f2d02bf32479b63c6786af788c7846dce
blogscot <iain.diamond@gmail.com>
2024-12-05 11:54:46 +0000

    Refactor day 5 solution

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,22 +11,19 @@ impl Rule {
     fn check(&self, update: &[u32]) -> bool {
         let index1 = update.iter().position(|&x| x == self.0);
         let index2 = update.iter().position(|&x| x == self.1);
-        match (index1, index2) {
-            (Some(a), Some(b)) => a < b,
-            _ => true,
+        if let (Some(a), Some(b)) = (index1, index2) {
+            return a < b
         }
+        true
     }
 
-    fn fix(&self, update: &mut Vec<u32>) {
+    fn fix(&self, update: &mut [u32]) {
         let index1 = update.iter().position(|&x| x == self.0);
         let index2 = update.iter().position(|&x| x == self.1);
-        match (index1, index2) {
-            (Some(a), Some(b)) => {
-                if b < a {
-                    update.swap(a, b);
-                }
+        if let (Some(a), Some(b)) = (index1, index2) {
+            if b < a {
+                update.swap(a, b);
             }
-            _ => {}
         }
     }
 }
@@ -35,35 +32,33 @@ fn check_rules(rules: &[Rule], update: &[u32]) -> bool {
     rules.iter().all(|rule| rule.check(update))
 }
 
-fn fix_rules(rules: &[Rule], update: &mut Vec<u32>) {
+fn fix_rules(rules: &[Rule], update: &mut [u32]) {
     rules.iter().for_each(|rule| rule.fix(update));
 }
 
 fn solve_part1(rules: &[Rule], updates: &[Vec<u32>]) -> u32 {
     updates
         .iter()
-        .filter(|update| check_rules(&rules, update))
+        .filter(|update| check_rules(rules, update))
         .map(|update| update[(update.len() - 1) / 2])
         .sum()
 }
 
-fn solve_part2(rules: &[Rule], updates: &mut [Vec<u32>]) -> u32 {
-    let mut failing: Vec<&mut Vec<u32>> = updates
-        .iter_mut()
-        .filter(|update| !check_rules(&rules, update))
+fn solve_part2(rules: &[Rule], updates: &[Vec<u32>]) -> u32 {
+    let mut failing: Vec<Vec<u32>> = updates
+        .iter()
+        .filter(|update| !check_rules(rules, update))
+        .cloned()
         .collect();
 
     failing
         .iter_mut()
-        .for_each(|update| {
-            while !check_rules(&rules, update) {
-                fix_rules(&rules, update)
+        .map(|update | {
+            while !check_rules(rules, update) {
+                fix_rules(rules, update)
             }
-        });
-
-    failing
-        .iter()
-        .map(|update| update[(update.len() - 1) / 2])
+            update[(update.len() - 1) / 2]
+        })
         .sum()
 }
 
@@ -81,7 +76,5 @@ fn main() {
         .collect();
 
     println!("Part 1: {:?}", solve_part1(&rules, &updates));
-
-    let mut updates: Vec<_> = updates;
-    println!("Part 2: {:?}", solve_part2(&rules, &mut updates));
+    println!("Part 2: {:?}", solve_part2(&rules, &updates));
 }
