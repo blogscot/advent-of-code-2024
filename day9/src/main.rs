2eae98b1e32206ac5fd6664c0aa5daf4a3dbbf7c
blogscot <iain.diamond@gmail.com>
2024-12-11 11:48:19 +0000

    Add day 9 part 2 solution

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Block {
     id: Option<u32>,
     size: u32,
@@ -34,7 +34,7 @@ fn _print_diskmap(diskmap: &VecDeque<Block>) {
     println!()
 }
 
-// Pop the next allocated block from the diskmap
+// Pop the next allocated block from the diskmap, discarding free blocks
 fn pop_back(diskmap: &mut VecDeque<Block>) -> Option<Block> {
     if let Some(block) = diskmap.pop_back() {
         if block.id.is_none() {
@@ -44,6 +44,35 @@ fn pop_back(diskmap: &mut VecDeque<Block>) -> Option<Block> {
         }
     }
     None
+}
+
+fn find_free_block_position(diskmap: &VecDeque<Block>, size: u32, limit: usize) -> Option<usize> {
+    for (i, block) in diskmap.iter().enumerate() {
+        if i >= limit {
+            return None;
+        }
+        if block.id.is_none() && block.size >= size {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn remaining_free_blocks(diskmap: &VecDeque<Block>, before: usize) -> bool {
+    (0..before).any(|i| diskmap[i].id.is_none())
+}
+
+fn checksum(compact: &VecDeque<Block>) -> u64 {
+    compact
+        .iter()
+        .flat_map(|b| {
+            std::iter::repeat(b.id.unwrap_or_default())
+                .take(b.size as usize)
+                .collect::<Vec<u32>>()
+        })
+        .enumerate()
+        .map(|(i, d)| (i as u32 * d) as u64)
+        .sum()
 }
 
 fn solve_part1(diskmap: &mut VecDeque<Block>) -> u64 {
@@ -86,17 +115,43 @@ fn solve_part1(diskmap: &mut VecDeque<Block>) -> u64 {
         }
     }
 
-    compact
-        .into_iter()
-        .flat_map(|b| {
-            std::iter::repeat(b.id.unwrap())
-                .take(b.size as usize)
-                .collect::<Vec<u32>>()
-        })
-        .enumerate()
-        .map(|(i, d)| (i as u32 * d) as u64)
-        .sum()
+    checksum(&compact)
+}
 
+fn solve_part2(diskmap: &mut VecDeque<Block>) -> u64 {
+    let mut current_block_index = diskmap.len() - 1;
+    while remaining_free_blocks(diskmap, current_block_index) {
+        while diskmap[current_block_index].id.is_none() {
+            current_block_index -= 1;
+        }
+        let allocated_block = diskmap[current_block_index];
+        if let Some(free_index) =
+            find_free_block_position(diskmap, allocated_block.size, current_block_index)
+        {
+            let free_block = diskmap[free_index];
+            match free_block.size.cmp(&allocated_block.size) {
+                std::cmp::Ordering::Equal => {
+                    diskmap[free_index].id = allocated_block.id;
+                    diskmap[current_block_index].id = None;
+                }
+                std::cmp::Ordering::Less => {
+                    panic!("free block is smaller than allocated block");
+                }
+                std::cmp::Ordering::Greater => {
+                    // move allocated block to free block
+                    // mark allocated block as free
+                    // create new free block with remaining free space
+                    let extra_space = free_block.size - allocated_block.size;
+                    diskmap[free_index].id = allocated_block.id;
+                    diskmap[free_index].size = allocated_block.size;
+                    diskmap[current_block_index].id = None;
+                    diskmap.insert(free_index + 1, reserve(extra_space));
+                }
+            }
+        }
+        current_block_index -= 1;
+    }
+    checksum(diskmap)
 }
 
 fn main() {
@@ -116,5 +171,6 @@ fn main() {
         }
     }
 
-    println!("Part 1: {:?}", solve_part1(&mut diskmap));
+    println!("Part 1: {:?}", solve_part1(&mut diskmap.clone()));
+    println!("Part 2: {:?}", solve_part2(&mut diskmap));
 }
