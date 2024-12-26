db0ef7ec08d5eb6295cbc340c13887e2b881a42f
blogscot <iain.diamond@gmail.com>
2024-12-26 22:02:08 +0000

    Add day 19 solution

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,21 +1,35 @@
-fn eat(design: &str, pattern: &str) -> Option<String> {
-    design
-        .strip_prefix(pattern)
-        .map(|stripped| stripped.to_string())
+use std::collections::HashMap;
+
+fn check(design: &str, patterns: &[&str]) -> usize {
+    if design.is_empty() {
+        return 1;
+    }
+    let count = 0;
+    for pattern in patterns {
+        if let Some(fabric) = design.strip_prefix(pattern) {
+            if check(fabric, patterns) == 1 {
+                return count + 1;
+            }
+        }
+    }
+    count
 }
 
-fn check(design: &str, patterns: &[&str]) -> bool {
+fn check2(cache: &mut HashMap<String, usize>, design: &str, patterns: &[&str]) -> usize {
     if design.is_empty() {
-        return true;
+        return 1;
     }
+    if let Some(count) = cache.get(design) {
+        return *count;
+    }
+    let mut count = 0;
     for pattern in patterns {
-        if let Some(fabric) = eat(design, pattern) {
-            if check(&fabric, patterns) {
-                return true;
-            }
-        };
+        if let Some(rest) = design.strip_prefix(pattern) {
+            count += check2(cache, rest, patterns);
+        }
     }
-    false
+    cache.insert(design.to_string(), count);
+    count
 }
 
 fn main() {
@@ -23,10 +37,15 @@ fn main() {
     let patterns = patterns.split(", ").collect::<Vec<_>>();
     let designs = designs.split("\n").collect::<Vec<_>>();
 
-    let result: usize = designs
-        .into_iter()
-        .filter(|design| check(design, &patterns))
-        .count();
+    let part1: usize = designs.iter().map(|design| check(design, &patterns)).sum();
 
-    println!("Part 1: {:?}", result);
+    println!("Part 1: {:?}", part1);
+
+    let mut cache = HashMap::new();
+    let part2: usize = designs
+        .iter()
+        .map(|design| check2(&mut cache, design, &patterns))
+        .sum();
+
+    println!("Part 2: {:?}", part2);
 }
